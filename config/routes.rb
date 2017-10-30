cc923ae875b62fd63064d390cc3fb1c1b6f784b8
shenyajun <980574369@qq.com>
2017-10-30 20:18:58 +0800

    Add account activation

--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  resources:users
+  resources :users
+  resources :account_activations,only:[:edit]
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+def setup
+  ActionMailer::Base.deliveries.clear
+end
+
 test "invalid signup information" do
   get signup_path
   assert_no_difference 'User.count' do
@@ -21,12 +25,22 @@ end
 test "valid signup infromation" do
   get signup_path
   assert_difference 'User.count',1 do
-    post users_path,params:{user:{name:'peiyanli',email:'peiyanli@xiaozhu.com',
+    post users_path,params:{user:{name:'peiyanli',email:'peiyanli@xiaozh.com',
 				  password:'hahaha',password_confirmation:'hahaha'}}
   end
+  assert_equal 1,ActionMailer::Base.deliveries.size
+  user = assigns(:user)
+  assert_not user.activated?
+  log_in_as(user)
+  assert_not is_logged_in?
+  get edit_account_activation_path("invalid token",email:user.email)
+  assert_not is_logged_in?
+  get edit_account_activation_path(user.activation_token,email:'wrong')
+  assert_not is_logged_in?
+  get edit_account_activation_path(user.activation_token,email:user.email)
+  assert user.reload.activated?
   follow_redirect!
   assert_template 'users/show'
-  assert_not flash.empty?
   assert is_logged_in?
 end
 end
